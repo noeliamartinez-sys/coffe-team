fddbb615ffdbf7a319f32508394c215f95556911
Facundo Alonzo <148971786+FacuAlonzo@users.noreply.github.com>
2026-05-15 20:46:05 -0300

    Estructura inicial del Banner con props

---
+++ src/Banner.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Banner.css';
+
+export default function Banner({ title, subtitle }) {
+  return (
+    <div className="banner">
+      <h1>{title}</h1>
+      <p>{subtitle}</p>
+    </div>
+  );
+}
